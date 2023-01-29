892ecefd425d5c5660013a815e1d45a56709bb6c
Arthur Le Floch <arthur.le_floch@enseirb-matmeca.fr>
2023-01-29 14:10:15 +0100

    Fixed previous commit not working properly with colors

--- src/utils.c
+++ src/utils.c
@@ -4,18 +4,18 @@ HSV_Color get_palette_color(SDL_Point * pos, SDL_Rect * palette){
     int x = pos->x - palette->x;
     int y = pos->y - palette->y;
 
-    float saturation = 0;
+    float value = 0;
     float hue = x / (float)palette->w;
-    float value = (1.0f - y / (float)palette->h);
+    float saturation = (1.0f - y / (float)palette->h);
     
     return (HSV_Color){hue, saturation, value};
 }
 
-SDL_Color get_sat_color(SDL_Point * pos, SDL_Rect * sat, HSV_Color color){
-    int x = pos->x - sat->x;
+SDL_Color get_lum_color(SDL_Point * pos, SDL_Rect * lum, HSV_Color color){
+    int x = pos->x - lum->x;
 
-    float saturation = x / (float)sat->w;
+    float luminosity = x / (float)lum->w;
     
-    HSV_Color result = {color.h, saturation, color.v};
+    HSV_Color result = {color.h, color.s, luminosity};
     return hsv_to_rgb(result);
 }
--- src/utils.h
+++ src/utils.h
@@ -5,6 +5,6 @@
 
 HSV_Color get_palette_color(SDL_Point * pos, SDL_Rect * palette);
 
-SDL_Color get_sat_color(SDL_Point * pos, SDL_Rect * sat, HSV_Color color);
+SDL_Color get_lum_color(SDL_Point * pos, SDL_Rect * lum, HSV_Color color);
 
 #endif // __UTILS_H__
